62d64deac291edeaafd20dde9425983b724b591d
PythonSaubhagyam <parth.saubhagyam@gmail.com>
2022-01-19 19:00:41 +0530

    add live db connection

--- test.py
+++ test.py
@@ -1,13 +1,60 @@
+# from traceback import print_tb
+# import pytesseract
+# import os
+# from PIL import Image
+# path ="/home/parth/Documents/DocSenderAPI/images"
+# def ocr_core():
+#     for imageName in os.listdir(path):
+#         inputPath = os.path.join(path, imageName)
+#         img = Image.open(inputPath)
+#         text =pytesseract.image_to_string(img)
+#         print(text)
+#         # print(text)# We'll use Pillow's Image class to open the image and pytesseract to detect the string in the image
+#     # return text
+# ocr_core()
+# from mysql.connector.constants import ClientFlag
+import mysql.connector
 
-from PIL import Image
-from io import BytesIO
-import base64
-from datetime import datetime
-import io
+# mydb =  mysql.connector.connect(
+# 	host = "localhost",
+# 	database='docsender_db',
+#     port = 3306,
+# 	user = "doc_admin",
+# 	password = "Admin123!@#"
+# )
+# cursor = mydb.cursor()
+# print(cursor)
 
-file= BytesIO(base64.b64decode("/home/parth/Documents/DocSenderAPI/upload_images/sample1.png"))
-# now=datetime.now()
-# ext='.jpeg'
-# filename = secure_filename(str(now)+ext)
-file_converted=base64.b64decode(file)
-image=Image.open(io.BytesIO(file_converted))
+
+# conn = MySQLdb.connect("host", "user", "password", "database")
+
+from sqlalchemy import create_engine
+
+
+
+username = 'doc_admin'
+password = 'Admin123!#'
+host = 'localhost'
+port = 3306
+DB_NAME = 'db_name'
+
+# engine = create_engine('mysql://{username}:{password}@{host}:{port}')
+# engine = create_engine('mysql+pymysql://doc_admin:Admin123!#@184.168.96.176/docsender_db')\
+# engine = create_engine('mysql+pymysql://db_user:db_user_pass@172.21.0.3/cart', pool_recycle=3600)
+# engine = create_engine('mysql+pymysql://doc_admin:Admin123!#@localhost')
+
+# Creating connection object
+# mydb =  mysql.connector.connect(
+# 	host = "localhost",
+# 	database='minipokemart',
+# 	user = "doc_admin",
+# 	password = "Admin1234!#$"
+# )
+# cursor = mydb.cursor()
+sqlEngine = create_engine('mysql+pymysql://doc_admin:Admin1234!#$@localhost:3306/docsender_db', pool_recycle=3600)
+dbConnection = sqlEngine.connect()
+# conn = engine.connect()
+# import sqlalchemy
+# engine = sqlalchemy.create_engine('mysql://doc_admin:Admin123!@#@sg2plzcpnl466820.prod.sin2.secureserver.net')
+# print(engine)
+# engine.connect()
