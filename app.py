23039eb7d7246307bf63759537b0d2b730a05e34
PythonSaubhagyam <parth.saubhagyam@gmail.com>
2022-01-20 15:35:09 +0530

    heroku live

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
-from codecs import IncrementalDecoder
-import imp
 from flask import Flask, request, jsonify
-from flask_restful import reqparse, Api, Resource,abort
+from flask_restful import Api, Resource
 import os 
 import pytesseract
 from PIL import Image
-from mysql.connector.constants import ClientFlag
-import mysql.connector
+# import mysql.connector
 import re
 from datetime import datetime
-import json
 
 app = Flask(__name__)
 api = Api(app)
@@ -25,13 +21,13 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__)) # path of the app
 # 	password="Admin1234!#$",
 # 	database="docsender_db"
 # 	)
-mydb =  mysql.connector.connect(
-	host = "localhost",
-	database='doc_sender',
-	user = "root",
-	password = ""
-)
-cursor = mydb.cursor()
+# mydb =  mysql.connector.connect(
+# 	host = "localhost",
+# 	database='doc_sender',
+# 	user = "root",
+# 	password = ""
+# )
+# cursor = mydb.cursor()
 
 # simple get method 
 class Index(Resource):
@@ -275,10 +271,10 @@ class OCRInfo(Resource):
                 gst,amount,contact_no,email,date,invoice_no,name=data[0],data[1],data[2],data[3],data[4],data[5],data[6]
                 data={"user_id":user_id,"gst_no":gst,"Total_amnt":amount,"Contact_no":contact_no,"email_id":email,"date":date,"invoice_number":invoice_no,"name":name}
                 user_list.append(data)
-                sql_query = "INSERT INTO doc_sender_user(user_id,filepath,filename,gst_no,total_amount,contact_no,email_id,date,invoice_number,doc_name) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)" # Insert values into database
-                values= (user_id,destination,file_name,gst,amount,contact_no,email,date,invoice_no,name)
-                cursor.execute(sql_query, values)
-                mydb.commit()
+                # sql_query = "INSERT INTO doc_sender_user(user_id,filepath,filename,gst_no,total_amount,contact_no,email_id,date,invoice_number,doc_name) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)" # Insert values into database
+                # values= (user_id,destination,file_name,gst,amount,contact_no,email,date,invoice_no,name)
+                # cursor.execute(sql_query, values)
+                # mydb.commit()
                 # json_object=json.dumps(data)
                 # json_object = json.loads(json_object.replace("\'", '"'))
             
